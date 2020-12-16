656e3413ced71931eabe72961bbf12ad72131fa7
tongdung13 <tongvanthienvu@gmail.com>
2020-12-16 08:39:44 +0700

    ok

---
+++ route.php
@@ -0,0 +1,8 @@
+
+   các cách viết route
+   Route::get()           Route::group        Route::view
+   Route::post            Route::patch        Route::redirect
+   Route::put             Route::delete       Route::middleware
+   Route::prefix          Route::match        Route::fallback
+   Route::name            Route::any
+   Route::namespace       Route::domain
---
+++ sosanh.php
@@ -0,0 +1,6 @@
+
+
+    Route::profix có thể sủ dụng để tiền tố mỗi tuyến đường trong nhóm với 1 ủi nhất định
+    Route::group được dùng để gom các loại route khác có chung 1 đặc điểm gì đó
+    mà bạn muốn đưa chúng và một nhóm
+
